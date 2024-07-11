6235e6355660611ffdf0d0d27d9f7aae832803ae
Gurottesque <a.bedoya3@utp.edu.co>
2024-07-11 17:21:44 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,6 @@ const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 const corsOptions = {
-  'Access-Control-Allow-Origin': 'http://localhost:3000',
   origin: 'http://localhost:3000', //Momentaneamente aceptara requests de cualquier lugar, cambiar en produccion
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type'],
